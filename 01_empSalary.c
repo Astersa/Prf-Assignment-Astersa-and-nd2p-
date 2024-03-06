7a6c3ec6b6be7817727b151d05367562c669d013
Phuong <ndpp.work@gmail.com>
2024-03-06 22:58:41 +0700

    update mode 1.

--- 01_empSalary.c
+++ 01_empSalary.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-
+#include <string.h>
+#include <ctype.h>
 #define Max 50
 
 struct employee{
-    long id,hours;
+    long id;
     long salary;
-    bool gender;
+    char gender[Max];
     char firstName[Max];
-    char secondName[Max];
+    char lastName[Max];
 };
 
 void take_data();
 void salary_cal();
+
+
+
+
 int main()
 {
     int employee_amount;
@@ -22,26 +25,41 @@ int main()
     take_data(employee_amount);
     return 0;
 }
+
+
+
+
 void salary_cal(int id, int hours, long salary)
 {    
     float total_salary = (float)salary*hours;
     printf("Emplyees ID: %d\n",id);
     printf("Salary = U$ %.2f\n",total_salary);
 }
+
+
+
+
 void take_data(int employee_amount)    
 {
     struct employee employees[employee_amount];
     int i = 0;
     for (i; i<employee_amount;i++){
-        printf("Input the %d Employees ID: ",i+1);
-        scanf("%d",&employees[i].id);
-        printf("Input worked hours of the %d employee: ",i+1);
-        scanf("%d",&employees[i].hours);
-        printf("Input slary per hour of %d employee: ",i+1);
-        scanf("%ld",&employees[i].salary);
-        printf("Employee number: %d\n",i+1);
-        salary_cal(employees[i].id,employees[i].hours,employees[i].salary);
+        printf ("Input the %d Employee ID: ", i+1);
+        scanf ("%d",&employees[i].id);
+        
+        printf ("Input the %d Employee's last name: ", i+1);
+        scanf ("%s", employees[i].lastName);
+        
+        printf ("Input the %d Employee's first name: ", i+1);
+        scanf ("%s", employees[i].firstName);
+        
+        printf ("Input the %d Employee's gender: ", i+1);
+        scanf ("%s", employees[i].gender);
+        
+        printf ("Input the %d Employee's salary: ", i+1);
+        scanf ("%lf", &employees[i].salary);
     }
 }
+
 
 
